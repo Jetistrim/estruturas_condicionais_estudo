05895c156bfe632badbb25ca687bda2a3e8a833e
Jetistrim <paulogabriellima@hotmail.com>
2024-02-01 11:59:57 -0300

    start

---
+++ index.js
@@ -0,0 +1,22 @@
+modo.onchange = () => {
+    if (modo.value == "1") {
+        document.querySelector('body').style.backgroundColor = "black";
+    }
+    else if (modo.value == 2){
+        document.querySelector('body').style.backgroundColor = "red";
+    }
+    else if (modo.value == 3){
+        document.querySelector('body').style.backgroundColor = "green";
+    }
+    else if (modo.value == 4){
+        document.querySelector('body').style.backgroundColor = "blue";
+    }
+    else if (modo.value == 5){
+        document.querySelector('body').style.backgroundColor = "purple";
+    }
+    else if (modo.value == 6){
+        document.querySelector('body').style.backgroundColor = "brown";
+    }
+    else {document.querySelector('body').style.backgroundColor = "yellow";
+    }
+}
